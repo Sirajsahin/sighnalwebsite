cedbd5c4d35d652f8f83765bc750453df54cd25b
mac <mac@APPLEs-MacBook-Pro.local>
2024-10-28 11:32:16 +0530

    logo

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -12,10 +12,11 @@ const user = {
     "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
 };
 const navigation = [
-  { name: "Dashboard", href: "#", current: true },
-  { name: "Team", href: "#", current: false },
-  { name: "Projects", href: "#", current: false },
-  { name: "Calendar", href: "#", current: false },
+  { name: "Product", href: "#", current: true },
+  { name: "Features", href: "#", current: false },
+  { name: "Use Cases", href: "#", current: false },
+  { name: "Pricing", href: "#", current: false },
+  { name: "Contact", href: "#", current: false },
 ];
 const userNavigation = [
   { name: "Your Profile", href: "#" },
@@ -35,12 +36,8 @@ export default function Example() {
           <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
             <div className="flex h-16 justify-between items-center">
               {/* Logo */}
-              <div className="flex items-center">
-                <img
-                  alt="Your Company"
-                  src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600"
-                  className="h-8 w-auto"
-                />
+              <div className="flex items-center ">
+                <h1 className="text-sm sm:text-base xl:text-2xl">Sighnal</h1>
               </div>
 
               {/* Desktop Navigation */}
